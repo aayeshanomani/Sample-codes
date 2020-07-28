37df9d2a458fdafb1271f2d0871b44b806eef729
aayeshanomani <aayeshanomani@gmail.com>
2020-07-28 21:19:31 +0530

    Increase site traffic editted

--- webview.py
+++ webview.py
@@ -4,10 +4,12 @@ import time
 
 driver = webdriver.Chrome(executable_path="D:\Installed\chromedriver.exe")
 site = input("Enter site name: ")
-driver.get(site)
+driver.get(site.strip())
 rate = input("Number of seconds after which site is to be refreshed: ")
 refreshrate = int(rate)
-
+count = 0
 while True:
     time.sleep(refreshrate)
     driver.refresh()
+    count = count+1
+    print(count)
