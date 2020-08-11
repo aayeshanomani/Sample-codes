e449e81819aa6e8458abf64f20f9e40159218020
aayeshanomani <aayeshanomani@gmail.com>
2020-08-11 19:51:10 +0530

    Text to speech in web

---
+++ voice/voice.js
@@ -0,0 +1,14 @@
+function say(id)
+{
+    const message = document.getElementById(id).value;
+
+    var speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
